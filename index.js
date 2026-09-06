d012ab6be7d4c2cb06842137d4877b7f25c77098
Kartavya <136502449+kartavya-12@users.noreply.github.com>
2026-09-07 01:00:17 +0530

    21-8-26
    Updated comments for clarity and changed quotes in app.use.

--- index.js
+++ index.js
@@ -12,8 +12,8 @@
 //   const { des } = req.query;
 
 //   if (des) {
-//     const filtered = packages.filter(
-//       (pkg) => pkg.name.includes(des) || pkg.description.includes(des),
+//     const filtered = packages.filter((pkg) =>
+//       pkg.name.includes(des) || pkg.description.includes(des)
 //     );
 //     return res.json(filtered);
 //   }
@@ -35,12 +35,36 @@
 //   console.log(`Server is running on port ${port}`);
 // });
 
+
+
+//______________________________________________________________________________________________________________
+
+
+//req.header is to send token in the header and req.query is to send token in the query string.
+//res.send is to send a response from server back to the client.
+//  res.json is to send a JSON response back to the client. 
+// res.status is to set the HTTP status code of the response.
+//MVC ko use karke hume data ko organise karna hota hai.
+//  Model, View, Controller ka use karke hum data ko alag alag layers me organize karte hai.
+
+//model is database se data ko handle karta hai, (interact with db)
+// view is user interface ko handle karta hai aur user ko data dikhata hai.
+//  controller is handling logic between model and view ko handle karta hai.
+
+
+// 4 folders banate hai MVC me, model, view, controller aur routes.
+
+//_________________________________________________________________________________________________________________________
+
+//model - controller p - fir route p - fir index.js p - fir server start hoga aur client ko response milega.
+//this is bottom to top approach prefferable
+
 const express = require("express");
 const app = express();
 const tourRoute = require("./routes/tourRoute");
 
 app.use(express.json()); //app.use is for middleware , authentication and autherization purpose
-app.use("/api", tourRoute);
+app.use('/api', tourRoute);
 
 app.listen(7000, () => {
   console.log("Server is running on port 7000");
