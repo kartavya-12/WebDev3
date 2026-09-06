0e445de8b7db9ac5deabfd6fc6ec57a6bbdd9eb2
Kartavya <136502449+kartavya-12@users.noreply.github.com>
2026-09-07 00:59:50 +0530

    21-8-26
    Added new routes for searching, retrieving by ID, saving, and updating tours.

--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -1,6 +1,11 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const tourController = require("../controller/tourController");
+const tourController = require('../controller/tourController');
 
-router.get("/", tourController.getAllTours);
+router.get('/tours', tourController.getAllTours);
+router.get('/tours/search', tourController.getToursByQuery);
+router.get('/tours/:id', tourController.getTourById);
+router.post('/tours', tourController.save);
+router.put('/tours/:id', tourController.updateTour);
+
 module.exports = router;
