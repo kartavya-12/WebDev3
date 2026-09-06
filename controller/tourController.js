265c06ca19d75f58ba5b681ea05774bea67ce641
Kartavya <136502449+kartavya-12@users.noreply.github.com>
2026-09-07 00:16:35 +0530

    19-8-26

---
+++ controller/tourController.js
@@ -0,0 +1,9 @@
+const tourModel = require("../model/tourModel");
+
+const getAllTours = (req, res) => {
+  const tours = tourModel.getAllTours();
+  res.json(tours);
+};
+module.exports = {
+  getAllTours,
+};
