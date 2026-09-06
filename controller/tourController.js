42f8bb13e643ed235564223f656164f8856c5c6b
Kartavya <136502449+kartavya-12@users.noreply.github.com>
2026-09-07 00:58:14 +0530

    21-8-26

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,9 +1,44 @@
-const tourModel = require("../model/tourModel");
+const tourModel = require('../model/tourModel');
 
 const getAllTours = (req, res) => {
-  const tours = tourModel.getAllTours();
-  res.json(tours);
+    const tours = tourModel.getAll();
+    res.json(tours);
 };
+
+const getTourById = (req, res) => {
+    const id = parseInt(req.params.id);
+    const tour = tourModel.getById(id);
+    if (!tour) {
+        return res.status(404).json({ message: 'Tour Not Found' });
+    }
+    res.json(tour);
+};
+
+const getToursByQuery = (req, res) => {
+    const { query } = req.query;
+    const tours = tourModel.getByQuery(query);
+    res.json(tours);
+};
+
+const save = (req, res) => {
+    const tour = req.body;
+    tourModel.save(tour);
+    res.status(201).json({ message: 'Tour created successfully' });
+};
+const updateTour = (req, res) => {
+    const id = req.params.id;
+    const updatedData = req.body;
+    const updatedTour = tourModel.update(id, updatedData);
+    if (!updatedTour) {
+        return res.status(404).json({ message: 'Tour Not Found' });
+    }
+    res.json(updatedTour);
+};
+
 module.exports = {
-  getAllTours,
-};
+    getAllTours,
+    getTourById,
+    getToursByQuery,
+    save,
+    updateTour
+};  
